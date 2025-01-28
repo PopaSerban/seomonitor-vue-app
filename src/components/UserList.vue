2bc582037da654e9cdb7b8ef398bffed31888659
Popa Serban Alexandru <serban.popa@outlook.com>
2025-01-28 23:49:58 +0200

    UserList redesign

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,41 +1,141 @@
 <template>
-  <div class="user-list" v-if="users.length">
-    <h2>User List</h2>
-    <ul>
-      <li v-for="user in users" :key="user.id">
-        {{ user.name }}
-        <button @click="selectuser(user)">View Details</button>
-      </li>
-    </ul>
+  <div class="table-container">
+    <div class="transaction-table">
+      <div class="table-header">
+        <div class="col-profile">Profile</div>
+        <div class="col-name auto">Full Name</div>
+        <div class="col-action">Actions</div>
+      </div>
+      <div v-if="users.length === 0">
+        <LoadingTable class="table-rows" :rowsNumber="10"></LoadingTable>
+      </div>
+      <div v-else v-for="user in users" :key="user.id" class="table-row">
+        <div class="profile">
+          <span>{{ getInitials(user.name) }}</span>
+        </div>
+        <div class="name auto">{{ user.name }}</div>
+        <div class="action">
+          <button @click="showDetails(user)">More Details</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <style scoped>
-.user-list {
+.table-container {
+  display: flex;
+  justify-content: center;
+  margin: 1rem;
+  padding: 2rem;
+  overflow-x: auto;
+}
+
+.transaction-table {
+  display: grid;
+  width: 100%;
+  max-width: 100%;
+  border-radius: 8px;
   padding: 1rem;
+  overflow: hidden;
+  border: 1px solid #e0e0e0;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.table-header {
+  display: grid;
+  grid-template-columns: 100px auto 120px;
+  font-weight: bold;
+  padding: 12px;
+  align-items: center;
+  text-align: left;
 }
 
-li {
-  margin-bottom: 0.5rem;
+.table-row {
+  display: grid;
+  grid-template-columns: 100px auto 120px;
+  align-items: left;
+  padding: 12px;
+  border-bottom: 1px solid #ddd;
+}
+
+.profile {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 42px;
+  height: 42px;
+  border-radius: 50%;
+  background-color: #6c5ce7;
+  color: white;
+  font-weight: bold;
+  font-size: 14px;
+  text-transform: uppercase;
+}
+
+.name {
+  font-size: 16px;
+  color: #333;
+  text-align: left;
+}
+
+.auto {
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  padding-left: 12px;
+}
+
+.col-action {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.action {
+  text-align: right;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 button {
-  margin-left: 1rem;
+  padding: 6px 12px;
+  border: none;
+  border-radius: 16px;
+  background-color: #3fb883;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #369e71;
 }
 </style>
 <script>
+import LoadingTable from "./reusable/LoadingTable.vue";
+
 export default {
   name: "UserList",
+  components: {
+    LoadingTable,
+  },
   created() {
     this.$store.dispatch("fetchUsers");
   },
   computed: {
     users() {
       return this.$store.getters.loadedUsers;
+    },
+  },
+  methods: {
+    getInitials(name) {
+      return name
+        .split(" ")
+        .map((n) => n[0])
+        .join("")
+        .toUpperCase();
+    },
+    showDetails(transaction) {
+      alert(`Showing details for ${transaction.name}`);
     },
   },
 };
---
+++ src/components/reusable/LoadingTable.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="loading-grid">
+    <div v-for="i in rowsNumber" :key="i" class="loading-row">
+      <div class="circle loading-shimmer"></div>
+      <div class="line loading-shimmer"></div>
+      <div class="line loading-shimmer"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoadingTable",
+  props: {
+    rowsNumber: {
+      type: Number,
+      default: 10,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.loading-grid {
+  display: grid;
+  gap: 16px;
+}
+
+.loading-row {
+  display: grid;
+  grid-template-columns: 50px auto 180px;
+  align-items: center;
+  gap: 16px;
+}
+
+.loading-shimmer {
+  animation: shimmer 2s infinite linear;
+  background: linear-gradient(
+    to right,
+    rgba(63, 184, 131, 0.6) 4%,
+    rgba(47, 163, 111, 0.7) 25%,
+    rgba(63, 184, 131, 0.6) 36%
+  );
+  background-size: 1000px 100%;
+  border-radius: 16px;
+}
+
+/* Keyframes for shimmer animation */
+@keyframes shimmer {
+  0% {
+    background-position: -1000px 0;
+  }
+
+  100% {
+    background-position: 1000px 0;
+  }
+}
+
+.circle {
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+}
+
+.line {
+  height: 20px;
+  width: 100%;
+}
+</style>
